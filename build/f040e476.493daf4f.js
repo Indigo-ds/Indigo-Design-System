fa5ba0edd819fb4b802e31b9f087a8e25abea4d2
Indigo-ds <77474334+Indigo-ds@users.noreply.github.com>
2021-03-24 11:58:48 +0800

    Delete build directory

--- build/f040e476.493daf4f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{105:function(o){o.exports=JSON.parse('{"allTagsPath":"/Indigo-Design-System/build/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/Indigo-Design-System/build/blog/tags/facebook"}')}}]);
